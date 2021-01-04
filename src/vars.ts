0269dbd8169bfc5888286aa093f45ab2c2a7bc6e
BART! <bart@bartweb.cz>
2021-01-04 21:41:50 +0100

    feat(user-ratings): get all pages [experimental]

--- src/vars.ts
+++ src/vars.ts
@@ -1,5 +1,7 @@
-export const userRatingsUrl = (user: string | number): string =>
-  `https://new.csfd.cz/uzivatel/${encodeURIComponent(user)}/hodnoceni/`;
+export const userRatingsUrl = (user: string | number, page?: number): string =>
+  `https://new.csfd.cz/uzivatel/${encodeURIComponent(user)}/hodnoceni/${
+    page ? '?page=' + page : ''
+  }`;
 
 export const movieUrl = (movie: number): string =>
   `https://new.csfd.cz/film/${encodeURIComponent(movie)}`;
