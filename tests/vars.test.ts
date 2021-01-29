f6a8f23c3e81ac9bc460a4114728a685eaac9f43
BART! <bart@bartweb.cz>
2021-01-29 22:12:48 +0100

    test(vars): var urls

--- tests/vars.test.ts
+++ tests/vars.test.ts
@@ -1,8 +1,26 @@
-import { userRatingsUrl } from '../src/vars';
+import { movieUrl, searchUrl, userRatingsUrl } from '../src/vars';
 
-describe('Vars', () => {
+describe('Vars User Ratings', () => {
   test('Assemble User rating url', () => {
     const url = userRatingsUrl('912-bart');
     expect(url).toBe('https://new.csfd.cz/uzivatel/912-bart/hodnoceni/');
+  });
+  test('Assemble User rating. Page 2', () => {
+    const url = userRatingsUrl('912-bart', 2);
+    expect(url).toBe('https://new.csfd.cz/uzivatel/912-bart/hodnoceni/?page=2');
+  });
+});
+
+describe('Vars Movies', () => {
+  test('Assemble movieUrl', () => {
+    const url = movieUrl(535121);
+    expect(url).toBe('https://new.csfd.cz/film/535121');
+  });
+});
+
+describe('Vars Search', () => {
+  test('Assemble searchUrl', () => {
+    const url = searchUrl('matrix');
+    expect(url).toBe('https://new.csfd.cz/hledat/?q=matrix');
   });
 });
