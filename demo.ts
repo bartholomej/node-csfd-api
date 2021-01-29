17b8dc0dce584ca67a46de19d9aa88fb68f3c2b2
BART! <bart@bartweb.cz>
2021-01-29 18:46:20 +0100

    feat(demo): add demo examples

--- demo.ts
+++ demo.ts
@@ -1,24 +1,28 @@
-import { writeFile } from 'fs';
+// import { writeFile } from 'fs';
 import { csfd } from './src';
 
 // Parse movie
-// csfd.movie(535121).then((movie) => console.log(movie));
+csfd.movie(535121).then((movie) => console.log(movie));
 
-// Save data in file
-const userId = 912;
+/**
+ * USER RATINGS
+ */
 
-csfd
-  .userRatings(userId, {
-    excludes: ['epizoda', 'TV pořad', 'série'],
-    allPages: true,
-    allPagesDelay: 2000
-  })
-  .then((ratings) => {
-    console.log('Saved in file:', `./${userId}.json`);
-    writeFile(`${userId}.json`, JSON.stringify(ratings), (err) => {
-      if (err) return console.log(err);
-    });
-  });
+// // Save all pages in json file
+// const userId = 912;
+//
+// csfd
+//   .userRatings(userId, {
+//     excludes: ['epizoda', 'TV pořad', 'série'],
+//     allPages: true,
+//     allPagesDelay: 2000
+//   })
+//   .then((ratings) => {
+//     console.log('Saved in file:', `./${userId}.json`);
+//     writeFile(`${userId}.json`, JSON.stringify(ratings), (err) => {
+//       if (err) return console.log(err);
+//     });
+//   });
 
 // // Only TV series
 // csfd
