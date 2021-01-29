c5b0b56c6fd82f02120439a5df300e3e7f02ca88
BART! <bart@bartweb.cz>
2021-01-29 18:20:50 +0100

    test(user-ratings): test pages

--- tests/fetchers.test.ts
+++ tests/fetchers.test.ts
@@ -5,11 +5,18 @@ const badId = 9999999999999999999;
 describe('Fetch rating page', () => {
   test('Fetch `912-bart` user', async () => {
     const html = await fetchUserRatings(912);
-    expect(html).toContain('Oeconomia');
+    expect(html).toContain('Vlkochodci');
   });
 });
 
-describe('Fetch rating page', () => {
+describe('Fetch rating page 2', () => {
+  test('Fetch `912-bart` user – page 2', async () => {
+    const html = await fetchUserRatings(912, 2);
+    expect(html).toContain('Neviditelný');
+  });
+});
+
+describe('Fetch movie page', () => {
   test('Fetch `535121-na-spatne-strane` movie', async () => {
     const html = await fetchMovie(535121);
     expect(html).toContain('Na špatné straně');
