deb810854cabde1de5731ce46afe903e7e609b21
BART! <bart@bartweb.cz>
2021-05-17 23:04:42 +0100

    feat(app): switch to csfd 2021 url

--- src/vars.ts
+++ src/vars.ts
@@ -1,10 +1,10 @@
 export const userRatingsUrl = (user: string | number, page?: number): string =>
-  `https://new.csfd.cz/uzivatel/${encodeURIComponent(user)}/hodnoceni/${
+  `https://www.csfd.cz/uzivatel/${encodeURIComponent(user)}/hodnoceni/${
     page ? '?page=' + page : ''
   }`;
 
 export const movieUrl = (movie: number): string =>
-  `https://new.csfd.cz/film/${encodeURIComponent(movie)}`;
+  `https://www.csfd.cz/film/${encodeURIComponent(movie)}`;
 
 export const searchUrl = (text: string): string =>
-  `https://new.csfd.cz/hledat/?q=${encodeURIComponent(text)}`;
+  `https://www.csfd.cz/hledat/?q=${encodeURIComponent(text)}`;
--- tests/vars.test.ts
+++ tests/vars.test.ts
@@ -3,24 +3,24 @@ import { movieUrl, searchUrl, userRatingsUrl } from '../src/vars';
 describe('Vars User Ratings', () => {
   test('Assemble User rating url', () => {
     const url = userRatingsUrl('912-bart');
-    expect(url).toBe('https://new.csfd.cz/uzivatel/912-bart/hodnoceni/');
+    expect(url).toBe('https://www.csfd.cz/uzivatel/912-bart/hodnoceni/');
   });
   test('Assemble User rating. Page 2', () => {
     const url = userRatingsUrl('912-bart', 2);
-    expect(url).toBe('https://new.csfd.cz/uzivatel/912-bart/hodnoceni/?page=2');
+    expect(url).toBe('https://www.csfd.cz/uzivatel/912-bart/hodnoceni/?page=2');
   });
 });
 
 describe('Vars Movies', () => {
   test('Assemble movieUrl', () => {
     const url = movieUrl(535121);
-    expect(url).toBe('https://new.csfd.cz/film/535121');
+    expect(url).toBe('https://www.csfd.cz/film/535121');
   });
 });
 
 describe('Vars Search', () => {
   test('Assemble searchUrl', () => {
     const url = searchUrl('matrix');
-    expect(url).toBe('https://new.csfd.cz/hledat/?q=matrix');
+    expect(url).toBe('https://www.csfd.cz/hledat/?q=matrix');
   });
 });
