5ab0202f7628b4c3d5f2ce5be5cfe17d5e0f2d40
BART! <bart@bartweb.cz>
2021-06-29 22:55:20 +0200

    test: add live csfd tests

--- tests/fetchers.test.ts
+++ tests/fetchers.test.ts
@@ -1,14 +1,15 @@
+import { csfd } from '../src';
 import { fetchPage } from '../src/fetchers';
-import { movieUrl, searchUrl, userRatingsUrl } from '../src/vars';
+import { movieUrl, userRatingsUrl } from '../src/vars';
 
 const badId = 999999999999999;
 
 // User Ratings
 describe('Fetch rating page', () => {
   test('Fetch `912-bart` user', async () => {
-    const url = userRatingsUrl(912);
-    const html = await fetchPage(url);
-    expect(html).toContain('Palm Springs');
+    const movies = await csfd.userRatings('912-bart');
+    expect(movies.map((x) => x.title)).toEqual(expect.arrayContaining(['Palm Springs']));
+    expect(movies.length).toEqual(50);
   });
 });
 
@@ -21,20 +22,30 @@ describe('Fetch rating page 2', () => {
 });
 
 // Movie
-describe('Fetch movie page', () => {
-  test('Fetch `535121-na-spatne-strane` movie', async () => {
-    const url = movieUrl(535121);
-    const html = await fetchPage(url);
-    expect(html).toContain('Dragged Across Concrete');
+describe('Live: Movie page', () => {
+  test('Fetch `10135-forrest-gump` movie', async () => {
+    const movie = await csfd.movie(10135);
+    expect(movie.title).toEqual<string>('Forrest Gump');
+    expect(movie.rating).toBeGreaterThan(90);
+    expect(movie.creators.directors[0].name).toEqual('Robert Zemeckis');
+    expect(movie.origins[0]).toEqual<string>('USA');
+  });
+  test('Fetch `71924-kralovstvi` serial years', async () => {
+    const movie = await csfd.movie(71924);
+    expect(movie.year).toEqual<string>('1994–1997');
+    expect(movie.title).toEqual<string>('Království');
   });
 });
 
 // Search
-describe('Fetch movie page', () => {
-  test('Fetch `535121-na-spatne-strane` movie', async () => {
-    const url = searchUrl('matrix');
-    const html = await fetchPage(url);
-    expect(html).toContain('Matrix');
+describe('Live: Search', () => {
+  test('Search matrix', async () => {
+    const search = await csfd.search('matrix');
+    const matrix = search.movies.find((x) => x.title === 'The Matrix');
+    expect(matrix?.year).toEqual<string>('1999');
+    expect(matrix?.creators?.directors.map((x) => x.name)).toEqual<string[]>(
+      expect.arrayContaining(['Lilly Wachowski'])
+    );
   });
 });
 
