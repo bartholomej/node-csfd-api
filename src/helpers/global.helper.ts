4090fb775f1533339a3d8afe558d7d98b73e6d13
BART! <bart@bartweb.cz>
2022-02-03 18:13:42 +0100

    fix(movie): handle duration

--- src/helpers/global.helper.ts
+++ src/helpers/global.helper.ts
@@ -40,3 +40,23 @@ export const parseColor = (quality: Colors): CSFDColorRating => {
 export const addProtocol = (url: string): string => {
   return url.startsWith('//') ? 'https:' + url : url;
 };
+
+export const parseISO8601Duration = (iso: string): number => {
+  const iso8601DurationRegex =
+    /(-)?P(?:([.,\d]+)Y)?(?:([.,\d]+)M)?(?:([.,\d]+)W)?(?:([.,\d]+)D)?T(?:([.,\d]+)H)?(?:([.,\d]+)M)?(?:([.,\d]+)S)?/;
+
+  const matches = iso.match(iso8601DurationRegex);
+
+  const duration = {
+    sign: matches[1] === undefined ? '+' : '-',
+    years: matches[2] === undefined ? 0 : matches[2],
+    months: matches[3] === undefined ? 0 : matches[3],
+    weeks: matches[4] === undefined ? 0 : matches[4],
+    days: matches[5] === undefined ? 0 : matches[5],
+    hours: matches[6] === undefined ? 0 : matches[6],
+    minutes: matches[7] === undefined ? 0 : matches[7],
+    seconds: matches[8] === undefined ? 0 : matches[8]
+  };
+
+  return +duration.minutes;
+};
