75f9a397e0a5308df7fb7bcfde86dd2ab19b16c3
BART! <bart@bartweb.cz>
2023-01-30 09:44:42 +0100

    feat(search): parsing tv series

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -23,12 +23,19 @@ export class SearchScraper {
     const html = parse(response);
     const moviesNode = html.querySelectorAll('.main-movies article');
     const usersNode = html.querySelectorAll('.main-users article');
-    return this.parseSearch(moviesNode, usersNode);
+    const tvSeriesNode = html.querySelectorAll('.main-series article');
+
+    return this.parseSearch(moviesNode, usersNode, tvSeriesNode);
   }
 
-  private parseSearch(moviesNode: HTMLElement[], usersNode: HTMLElement[]) {
+  private parseSearch(
+    moviesNode: HTMLElement[],
+    usersNode: HTMLElement[],
+    tvSeriesNode: HTMLElement[]
+  ) {
     const movies: CSFDSearchMovie[] = [];
     const users: CSFDSearchUser[] = [];
+    const tvSeries: CSFDSearchMovie[] = [];
 
     moviesNode.map((m) => {
       const url = getUrl(m);
@@ -63,10 +70,30 @@ export class SearchScraper {
       users.push(user);
     });
 
+    tvSeriesNode.map((m) => {
+      const url = getUrl(m);
+
+      const user: CSFDSearchMovie = {
+        id: parseIdFromUrl(url),
+        title: getTitle(m),
+        year: getYear(m),
+        url: `https://www.csfd.cz${url}`,
+        type: getType(m),
+        colorRating: getColorRating(m),
+        poster: getPoster(m),
+        origins: getOrigins(m),
+        creators: {
+          directors: parsePeople(m, 'directors'),
+          actors: parsePeople(m, 'actors')
+        }
+      };
+      tvSeries.push(user);
+    });
+
     const search: CSFDSearch = {
       movies: movies,
       users: users,
-      tvSeries: [],
+      tvSeries: tvSeries,
       creators: []
     };
     return search;
