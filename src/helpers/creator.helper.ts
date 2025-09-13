1baefd38af4c65e2debe586f0d0677489819204e
BART! <bart@bartweb.cz>
2025-09-13 10:34:11 +0200

    refactor(creator): specific method names

--- src/helpers/creator.helper.ts
+++ src/helpers/creator.helper.ts
@@ -4,22 +4,22 @@ import { CSFDColorRating } from '../interfaces/global';
 import { Colors } from '../interfaces/user-ratings.interface';
 import { addProtocol, parseColor, parseIdFromUrl } from './global.helper';
 
-export const getColorRating = (el: HTMLElement): CSFDColorRating => {
+const getCreatorColorRating = (el: HTMLElement): CSFDColorRating => {
   return parseColor(el?.classNames.split(' ').pop() as Colors);
 };
 
-export const getId = (url: string): number => {
+export const getCreatorId = (url: string): number => {
   if (url) {
     return parseIdFromUrl(url);
   }
   return null;
 };
 
-export const getName = (el: HTMLElement | null): string => {
+export const getCreatorName = (el: HTMLElement | null): string => {
   return el.querySelector('h1').innerText.trim();
 };
 
-export const getBirthdayInfo = (
+export const getCreatorBirthdayInfo = (
   el: HTMLElement | null
 ): { birthday: string; age: number; birthPlace: string } => {
   const infoBlock = el.querySelector('h1 + p');
@@ -42,35 +42,35 @@ export const getBirthdayInfo = (
   return { birthday, age, birthPlace };
 };
 
-export const getBio = (el: HTMLElement | null): string => {
+export const getCreatorBio = (el: HTMLElement | null): string => {
   return el.querySelector('.article-content p')?.text.trim().split('\n')[0].trim() || null;
 };
 
-export const getPhoto = (el: HTMLElement | null): string => {
+export const getCreatorPhoto = (el: HTMLElement | null): string => {
   const image = el.querySelector('img').attributes.src;
   return addProtocol(image);
 };
 
-export const parseBirthday = (text: string): any => {
+const parseBirthday = (text: string): any => {
   return text.replace(/nar./g, '').trim();
 };
 
-export const parseAge = (text: string): any => {
+const parseAge = (text: string): any => {
   return text.trim().replace(/\(/g, '').replace(/let\)/g, '').trim();
 };
 
-export const parseBirthPlace = (text: string): any => {
+const parseBirthPlace = (text: string): any => {
   return text.trim().replace(/<br>/g, '').trim();
 };
 
-export const getFilms = (el: HTMLElement | null): CSFDCreatorScreening[] => {
+export const getCreatorFilms = (el: HTMLElement | null): CSFDCreatorScreening[] => {
   const filmNodes = el.querySelectorAll('.box')[0]?.querySelectorAll('table tr');
   let yearCache: number;
   const films = filmNodes.map((filmNode) => {
-    const id = getId(filmNode.querySelector('td.name .film-title-name')?.attributes.href);
+    const id = getCreatorId(filmNode.querySelector('td.name .film-title-name')?.attributes.href);
     const title = filmNode.querySelector('.name')?.text.trim();
     const year = +filmNode.querySelector('.year')?.text.trim();
-    const colorRating = getColorRating(filmNode.querySelector('.name .icon'));
+    const colorRating = getCreatorColorRating(filmNode.querySelector('.name .icon'));
 
     // Cache year from previous film because there is a gap between movies with same year
     if (year) {
