056086098f68276a8b41e865debd941f27a41fb7
BART! <bart@bartweb.cz>
2025-09-15 11:17:28 +0200

    feat(tv-series): seasons, episodes [kickoff]

--- src/dto/movie.ts
+++ src/dto/movie.ts
@@ -17,6 +17,14 @@ export interface CSFDMovie extends CSFDScreening {
   premieres: CSFDPremiere[];
   related: CSFDMovieListItem[];
   similar: CSFDMovieListItem[];
+  seasons: CSFDSeason[] | null;
+  episodes: CSFDSeason[] | null;
+  parent: CSFDParent | null;
+}
+
+export interface CSFDParent {
+  season: { id: number; name: string; };
+  series: { id: number; name: string; };
 }
 
 export type CSFDVodService =
@@ -86,6 +94,8 @@ export interface CSFDMovieListItem {
   url: string;
 }
 
+
+
 export type CSFDGenres =
   | 'Akční'
   | 'Animovaný'
@@ -150,3 +160,10 @@ export interface CSFDPremiere {
 }
 
 export type CSFDBoxContent = 'Související' | 'Podobné';
+
+export interface CSFDSeason {
+  id: number;
+  name: string;
+  url: string;
+  info: string | null;
+}
