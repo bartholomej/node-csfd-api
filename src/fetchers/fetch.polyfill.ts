e5911d2f07f7cf6e176a4cba7c4de4948afe70e8
BART! <bart@bartweb.cz>
2026-03-13 15:20:12 +0100

    chore(fetch): remove obsolete crossfetch

--- src/fetchers/fetch.polyfill.ts
+++ src/fetchers/fetch.polyfill.ts
@@ -1,7 +1,5 @@
-// Check if `fetch` is available in global scope (nodejs 18+) or in window (browser). If not, use cross-fetch polyfill.
-import { fetch as crossFetch } from 'cross-fetch';
+// Check if `fetch` is available in global scope (nodejs 18+) or in window (browser).
 export const fetchSafe =
   (typeof fetch === 'function' && fetch) || // ServiceWorker fetch (Cloud Functions + Chrome extension)
   (typeof global === 'object' && global.fetch) || // Node.js 18+ fetch
-  (typeof window !== 'undefined' && window.fetch) || // Browser fetch
-  crossFetch; // Polyfill fetch
+  (typeof window !== 'undefined' && window.fetch); // Browser fetch
